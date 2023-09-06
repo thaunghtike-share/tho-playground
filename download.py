8473f77684cd034e21a8134fd6036d7cfafcd96a
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2023-09-06 22:22:38 +0630

    Update download.py

--- download.py
+++ download.py
@@ -1,75 +1,26 @@
 import os
 import subprocess
-import time
-from tqdm import tqdm
-import itertools
 
-def stocks():
-    return json.load(open(stocks_json))
+def create_and_push_files():
+    for i in range(1, 6):
+        filename = f"file_{i}.txt"
+        with open(filename, "w") as file:
+            file.write(f"This is file {i}\n")
+        print(f"Created {filename}")
 
+        # Add the created file to the Git repository
+        subprocess.run(["git", "add", filename])
 
-def is_file_over_3_days_old(file_path):
-    # Get the modification time of the file
-    mod_time = os.path.getmtime(file_path)
+    # Commit the changes
+    subprocess.run(["git", "commit", "-m", "Add files"])
 
-    # Get the current time
-    current_time = time.time()
-
-    # Calculate the time difference in seconds
-    time_diff = current_time - mod_time
-
-    # Calculate the time difference in days
-    time_diff_days = time_diff / (24 * 60 * 60)
-
-    # Check if the file is over 3 days old
-    return time_diff_days > 3
-    
-def edgar_download():
-    datadir = os.path.expanduser("~/Dropbox/Family Room/data")
-    repo_path = "."
-
-    pbar = tqdm(itertools.islice(stocks(), 2))
-    for stock in pbar:
-        pbar.set_description(stock['Symbol'])
-        symbol = stock['Symbol'].replace('.', '-')
-
-        if stock['CIK'] is None:
-            continue
-
-        cik = "%010d" % stock['CIK']
-
-        file = os.path.join(
-            datadir,
-            f"data-sec-edgar/edgar/{symbol}.json"
-        )
-
-        if os.path.exists(file) and not is_file_over_3_days_old(file):
-            continue
-
-        # Download the file
-        download_command = f"curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json -o '{file}'"
-        print(subprocess.getoutput(download_command))
-
-        # Change the working directory to the Git repository root
-        os.chdir(repo_path)
-
-        # Add the downloaded file to the Git repository
-        git_add_command = f"git add {file}"
-        subprocess.getoutput(git_add_command)
-
-        # Commit the changes with a message
-        commit_message = f"Added {symbol}.json"
-        git_commit_command = f"git commit -m '{commit_message}'"
-        subprocess.getoutput(git_commit_command)
-
-        # Push the changes to the remote Git repository
-        git_push_command = f"git push origin master"  # Modify the branch name if needed
-        subprocess.getoutput(git_push_command)
-
-        # Change the working directory back to the original directory
-        os.chdir(datadir)
-
-        time.sleep(2)
+    # Push the changes to the repository
+    subprocess.run(["git", "push"])
 
 if __name__ == '__main__':
-    edgar_download()
+    # Change to your Git repository directory
+    # Make sure Git is configured with your name and email
+    subprocess.run(["git", "config", "user.name", "thaunghtike-share"])
+    subprocess.run(["git", "config", "user.email", "zwe.zzzin@email.com"])
+
+    create_and_push_files()
